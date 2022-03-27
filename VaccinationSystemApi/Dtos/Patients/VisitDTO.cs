7b715dd74f9775c65b7dca1754b10016cc0b4e45
Paweł Drabarek <01151568@pw.edu.pl>
2022-03-27 18:07:13 +0200

    added dto from earlier

---
+++ VaccinationSystemApi/Dtos/Patients/VisitDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace K_VaccinationSystem_backend.DTOs.Patient.Response
+{
+    public class VisitDTO
+    {
+        public string VaccineType { get; set; }
+        public string VaccineBatch { get; set; }
+        public int WhichVaccineDose { get; set; }
+        public string VisitId { get; set; }
+        public DateTime WindowBegin { get; set; }
+        public DateTime WindowEnd { get; set; }
+        public string VaccinationCenterName { get; set; }
+        public string VaccinationCenterCity { get; set; }
+        public string VaccinationCenterStreet { get; set; }
+    }
+}
