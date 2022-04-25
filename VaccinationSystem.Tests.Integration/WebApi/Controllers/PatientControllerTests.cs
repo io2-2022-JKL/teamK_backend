1473a3c677e9cc594936bbc612d5ba1c7f74a784
NorbertHanfPW <01151600@pw.edu.pl>
2022-04-25 08:45:45 +0200

    create inmem database and integration test for patient endpoint

---
+++ VaccinationSystem.Tests.Integration/WebApi/Controllers/PatientControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using VaccinationSystemApi;
+using FluentAssertions;
+using VaccinationSystemApi.Dtos.Patients;
+
+namespace VaccinationSystem.Tests.Integration.WebApi.Controllers
+{
+    [TestClass]
+
+    public class PatientControllerTests
+    {
+        private HttpClient _client;
+
+        public PatientControllerTests()
+        {
+            _client = new CustomWebApplicationFactory<Startup>().CreateClient();
+        }
+
+        [TestMethod]
+        public async Task BrowseVaccinationCenters_ReturnsListOfCentersInWarsaw_WhenExisting()
+        {
+            //arrange
+            var expectedNumberOfCenters = 1;
+
+            //act
+            var httpResponse = await _client.GetAsync("/centers/Warszawa");
+
+            var json = await httpResponse.Content.ReadAsStringAsync();
+            var vehicles = System.Text.Json.JsonSerializer.Deserialize<List<BrowseVaccinationCentersResponse>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            //assert
+            httpResponse.IsSuccessStatusCode.Should().BeTrue();
+            vehicles.Should().HaveCount(expectedNumberOfCenters);
+        }
+    }
+}
