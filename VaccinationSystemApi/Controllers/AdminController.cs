a580f4121ba93422109cda1d65bdc0e9fba448f8
Paweł Drabarek <01151568@pw.edu.pl>
2022-05-15 13:17:32 +0200

    created admin controller

---
+++ VaccinationSystemApi/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using VaccinationSystemApi.Dtos.Doctors;
+using VaccinationSystemApi.Repositories.Interfaces;
+using VaccinationSystemApi.Helpers;
+using VaccinationSystemApi.Models;
+
+namespace VaccinationSystemApi.Controllers
+{
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IVaccinationSystemRepository _vaccinationService;
+        public AdminController(IVaccinationSystemRepository repo) => _vaccinationService = repo;
+    
+        
+    }
+}
